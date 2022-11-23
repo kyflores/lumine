2693a22c6ece14f09f58c5b1d8a67d3693755a6c
Kyle Flores <6565728+kyflores@users.noreply.github.com>
2022-11-23 00:00:19 -1000

    Implement OpenCVDNN backend

--- src/detectors.py
+++ src/detectors.py
@@ -17,6 +17,12 @@ C310_PARAMS = (
     369.80679933903093,
 )
 
+# TODO: For OpenCV DNN
+BOX_THRESH = 0.25
+CLASS_THRESH = 0.25
+NMS_THRESH = 0.45
+NMS_SCORE_THRESH = BOX_THRESH * CLASS_THRESH
+
 # Relative to main.py
 YOLOV5_PATH = os.path.dirname(__file__) + "/subprojects/yolov5/"
 # Classes in the yolov5 model. Indexes should match the ultralytics/yolov5 pretrained models.
@@ -188,3 +194,106 @@ class YoloV5TorchDetector:
                 }
             )
         return res
+
+
+# Uses OpenCV dnn for inference. The model must be exported
+# to ONNX for this backend.
+# Also, the additional inference backends must be enabled in OpenCV
+# at compile time. IIRC opencv-python build on PyPi does NOT enable
+# cuda, vulkan, or openvino.
+class YoloV5OpenCVDetector:
+    def __init__(self, weights="yolov5s.onnx", classes=YOLOV5_CLASSES, backend="cpu"):
+        self.classes = classes
+        self.net = cv2.dnn.readNet(weights)
+        if backend == "vulkan":
+            print("Vulkan will be used if it is available.")
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_VKCOM)
+            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_VULKAN)
+        elif backend == "opencl":
+            print("OpenCL will be used if it is available.")
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL_FP16)
+        elif backend == "cuda":
+            print("CUDA will be used if it is available.")
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+        elif backend == "cpu":
+            print("CPU backend will be used.")
+        else:
+            print("Unknown backend, CPU backend will be used.")
+
+        self.out_names = self.net.getUnconnectedOutLayersNames()
+
+    def detect(self, img):
+        img = self._resize_to_frame(img)
+        blob = cv2.dnn.blobFromImage(
+            img,
+            1.0 / 255,
+            size=(img.shape[1], img.shape[0]),
+            mean=(0.0, 0.0, 0.0),
+            swapRB=False,
+            crop=False,
+        )
+
+        self.net.setInput(blob)
+        outs = self.net.forward(self.out_names)
+        (nms_res, boxes, confidences, class_ids) = self._process_net_out(outs)
+        res = []
+        for idx in nms_res:
+            conf = confidences[idx]
+            classnm = class_ids[idx]
+            x, y, w, h = np.clip(boxes[idx], 0, 640).astype(np.uint32)
+            d = (x, y, x + w, y + h)  # xyxy format
+            corners = np.array(((d[0], d[1]), (d[0], d[3]), (d[2], d[3]), (d[2], d[1])))
+
+            res.append(
+                {
+                    "type": "yolov5",
+                    "id": classnm,
+                    "color": (0, 255, 0),
+                    "corners": corners,
+                    "confidence": conf,
+                    "sort_xyxy": np.append(d[0:4], conf),
+                }
+            )
+        return res
+
+    def _process_net_out(self, tensor):
+        # 25200 is the total number of anchorboxes in the model output.
+        tns = np.array(tensor).reshape(25200, len(self.classes) + 5)
+        class_ids = np.empty(tns.shape[0])
+        confidences = np.empty(tns.shape[0])
+        boxes = np.empty((tns.shape[0], 4))
+
+        count = 0
+        for pred in tns[:]:
+            assert pred.shape[0] == 5 + len(self.classes)
+
+            class_scores = pred[5:]
+            score_idx = np.argmax(class_scores)
+            best_score = class_scores[score_idx]
+
+            if best_score >= CLASS_THRESH:
+                x, y, w, h, c = pred[:5]
+                left = x - 0.5 * w
+                top = y - 0.5 * h
+
+                rect = np.array((left, top, w, h))
+
+                class_ids[count] = score_idx
+                confidences[count] = best_score * c
+                boxes[count] = rect
+                count += 1
+
+        nms_res = cv2.dnn.NMSBoxes(
+            boxes[:count], confidences[:count], NMS_SCORE_THRESH, NMS_THRESH
+        )
+        return (nms_res, boxes[:count], confidences[:count], class_ids[:count])
+
+    def _resize_to_frame(self, imraw):
+        major_dim = np.max(imraw.shape)
+        scale = 640 / major_dim
+        imraw = cv2.resize(imraw, None, fx=scale, fy=scale)
+        img = np.zeros((640, 640, 3), dtype=imraw.dtype)
+        img[: imraw.shape[0], : imraw.shape[1], :] = imraw
+        return img
